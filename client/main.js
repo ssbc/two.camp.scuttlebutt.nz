fce3f1473c29194d43c70dff0716d6fe8e6ece4a
mix irving <whimful@gmail.com>
2016-08-27 19:15:36 +1200

    add map

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import html from 'yo-yo'
 import { Domain } from 'inux'
 
 import navigation from './partials/navigation'
+import map from './partials/map'
 
 const DOMAIN = 'main'
  
@@ -12,8 +13,8 @@ const view = (model, dispatch) => html`
 
     <a href='rsvp' class='button'>rsvp link</a> <br />
     January 10th 2016 <br />
-    Map <br />
 
+    ${map}
   </div>
 `
 
---
+++ client/partials/map.js
@@ -0,0 +1,11 @@
+import html from 'yo-yo'
+
+const map = html`
+
+<div class='map'>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3019.0838885953804!2d175.20496021598964!3d-40.82612102007757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d4095c904399351%3A0x33c144918b4a99a9!2sWaih%C5%8Danga+Centre+For+Well-being!5e0!3m2!1sen!2snz!4v1472280444664" width="500" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+`
+
+export default map
+
