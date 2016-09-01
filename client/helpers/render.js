4cc6afd02ab6b495526ce49e1bc4086f0991034f
mix irving <whimful@gmail.com>
2016-09-01 19:47:14 +1200

    minor

--- client/helpers/render.js
+++ client/helpers/render.js
@@ -4,15 +4,14 @@ import navigation from '../partials/navigation'
 
 const activePath = (model) => model.href.match(/[^\/]*$/)[0]
 
-export default (view) => (params, model, dispatch) => { 
-  return html`
-    <div>
-      ${navigation(activePath(model))}
-      <div class='content'>
-        ${view(params, model, dispatch)}
-      </div>
+export default (view) => (params, model, dispatch) => html`
+  <div>
+    ${navigation(activePath(model))}
+    <div class='content'>
+      ${view(params, model, dispatch)}
     </div>
-  `
-}
+  </div>
+`
+
 
 
