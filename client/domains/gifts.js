7efa793fa44e1d339721cb75edd06b2252bcbb2f
mix irving <whimful@gmail.com>
2017-01-28 13:53:34 +1300

    bank account name change

--- client/domains/gifts.js
+++ client/domains/gifts.js
@@ -31,7 +31,7 @@ const view = (params, model, dispatch) => html`
       Paypal: <pre>kdynamic@gmail.com</pre> <br />
 
       NZ Bank Transfer: <br />
-      <pre>A S KRAUSE, J A IRVING</pre><br />
+      <pre>A S & J A IRVING</pre><br />
       <pre>38-9014-0578605-03</pre>
     </div>
     
